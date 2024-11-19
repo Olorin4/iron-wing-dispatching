bef6d7f81049fe53be6f8b455308a5f23033dad3
Nick Kalas <nick.kalas@proton.me>
2024-11-19 21:53:01 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+
+export default [
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    eslintPluginPrettier,
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "error",
+            "arrow-body-style": ["error", "as-needed"],
+        },
+    },
+];
---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Hashmap",
+            template: path.resolve(__dirname, "./src/temp.html"),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/[name].[hash][ext][query]",
+                },
+            },
+            // {
+            //     test: /\.html$/,
+            //     use: ["html-loader"],
+            // },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+};
