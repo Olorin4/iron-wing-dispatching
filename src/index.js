a95bca366df99622ef0ddb5afa1c369367cc8af5
Nick Kalas <nick.kalas@proton.me>
2024-12-17 11:46:39 +0200

    Improve the CLS score slightly

--- src/index.js
+++ src/index.js
@@ -12,13 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // TO DO:
+// - Fix Cumulative Layout Shift
 // - add a second sales-pitch and two small images, one on the left of the 1st sales pitch, another on the right of the 2nd one.
 // - add a chatbot
 // - add more contact forms in the homepage
-// - Flesh out blog
+// - Add a smaller contact form prompt on he side of the sales pitch, to catch undecided clients.
+// - Add hover effects to the nav menu and the chat bot
 // - Fix responsiveness of sign up html
-//
-//
-//
-//
-//
+// - Flesh out blog
