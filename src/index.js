cae0e0f798fc320375ba83716c334b015c7b5885
Nick Kalas <nick.kalas@proton.me>
2024-12-18 18:17:02 +0200

    Add second sales pitch with image

--- src/index.js
+++ src/index.js
@@ -8,22 +8,18 @@ function logAndStoreMetric(name, value) {
     const metrics = JSON.parse(localStorage.getItem("web-vitals")) || {};
     metrics[name] = value;
     localStorage.setItem("web-vitals", JSON.stringify(metrics));
-
     console.log(`${name}:`, value);
 }
 
 // Capture Web Vitals metrics
+console.log("Web Vitals script is running.");
 onCLS((metric) => logAndStoreMetric("CLS", metric.value));
 onLCP((metric) => logAndStoreMetric("LCP", metric.value));
 onINP((metric) => logAndStoreMetric("INP", metric.value));
 
-console.log("Web Vitals script is running.");
-
-// DOMContentLoaded for Hamburger Menu
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".hamburger");
     const navLinks = document.querySelector("nav");
-
     hamburger.addEventListener("click", () => {
         navLinks.classList.toggle("active");
         console.log("Hamburger menu toggled");
@@ -31,11 +27,12 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // TO DO:
+// - Add more styling to the service and pricing sections.
 // - Fix Cumulative Layout Shift
-// - add a second sales-pitch and two small images, one on the left of the 1st sales pitch, another on the right of the 2nd one.
 // - add a chatbot
 // - add more contact forms in the homepage
 // - Add a smaller contact form prompt on he side of the sales pitch, to catch undecided clients.
 // - Add hover effects to the nav menu and the chat bot
+// - Fix about section affecting the footer
 // - Fix responsiveness of sign up html
 // - Flesh out blog
